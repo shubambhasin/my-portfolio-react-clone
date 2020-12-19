eb127c248d0be67c58545c6e169892ce4ba1eefa
Shubam Bhasin <33823641+shubambhasin@users.noreply.github.com>
2020-12-19 23:40:00 +0530

    feat: Updated UI and project section UI updated and new projects added in the project section

--- src/ProjectTiles.js
+++ src/ProjectTiles.js
@@ -6,20 +6,24 @@ import "./styles.css";
 const ProjectTiles = ({
   title = "Project",
   date = "Present Month",
-  tileBody = "Some text explaining the project"
+  tileBody = "Some text explaining the project",
+  smallText="Note:"
 }) => {
   return (
     <div>
       <div className="container container-center padding1rem container-radius bg-gray paddingbottom">
         <h3>
-          <strong style={{color:"darkorange"}}>{title}</strong>
+          <strong style={{ color: "#FF512F" }}>{title}</strong>
         </h3>
         <small>{date}</small>
 
         <p>{tileBody}</p>
-        <PrimaryButton buttonText="Live Project"/>
+        <small>{smallText}</small>
+        <br />
+        <br/>
+        <PrimaryButton buttonText="Live Project" />
         <span> </span>
-        <SecondaryButton buttonText="Github"/>
+        <SecondaryButton buttonText="Github" />
       </div>
       <br />
       <br />
--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ProjectTiles from "./ProjectTiles";
 import "./styles.css";
-import projects from "./projects.svg"
+import projects from "./projects.svg";
 
 // import PrimaryButton from "./PrimaryButton";
 // import SecondaryButton from "./SecondaryButton";
@@ -9,22 +9,84 @@ import projects from "./projects.svg"
 const Projects = () => {
   return (
     <div className="App">
-      <br/>
-      <br/>
+      <br />
+      <br />
       <header className="container container-center">
         <img class="header-images-medium" src={projects} alt="" />
       </header>
-      <br/>
-      <br/>
-      
+      <br />
+      <br />
 
-      <ProjectTiles />
+      <ProjectTiles
+        title="Astronomy picture of the day"
+        date="December 2020"
+        tileBody="This Web App will show you the APOD: Astronomy Picture of the Day, it fetches data via an API from"
+      />
+
+      <ProjectTiles
+        title="React clone of this Portfolio"
+        date="December 2020"
+        tileBody="This is an exact UI clone of this same website which you are on right now, but in React 😉 "
+      />
+      <ProjectTiles
+        title="Weather App"
+        date="December 2020"
+        tileBody="Dont know about today's weather at your place ?? Dont worry I got you covered. This is a Weather app, this
+            gives info like temperature, sunrise time, sunset time, humidity data and etc. "
+        smallText="Note: This App fetches data from an Api."
+      />
       <ProjectTiles
         title="Food recommentation App"
         date="December 2020"
         tileBody=" This is a food recommendation app, feel free to give reviews."
+        smallText="For more info click below"
       />
-     
+      <ProjectTiles
+        title="Singer Recommendation App"
+        date="December 2020"
+        tileBody="This App recommends the Singer based on the language you choose. It list the name of the Singer and One of their song. "
+        smallText="For more info click below"
+      />
+      <ProjectTiles
+        title="Country Flags Interpreter App"
+        date="December 2020"
+        tileBody=" This App can search Flags of any country for you. You can either enter country name or enter its flag emoji from key board, the app will show its flag or the
+            name of the country respectively "
+        smallText="Note:Enter the first letter of country name in Caps 😊, This App is built using ReactJS"
+      />
+
+      <ProjectTiles
+        title="Emoji Interpreter App"
+        date="December 2020"
+        tileBody="This is an Emoji Interpretation App, you enter an enomji, it will give you its meaning or you click any emoji
+            from the database it will show its meaning."
+        smallText="This App is build using ReactJS, Fr more info click below."
+      />
+      <ProjectTiles
+        title="Game of thrones translator"
+        date="December 2020"
+        tileBody="This is an App that takes english language as input and it gives the translation to Dothraki's language. I am
+            very sure Game of thrones fans gonna love it."
+        smallText="For more info click below "
+      />
+      <ProjectTiles
+        title="Minion Language Translator"
+        date="December 2020"
+        tileBody="This is English Langauge to Minion Language translator. This App uses  Fun Translation API to perform actions"
+        smallText="For more info click below "
+      />
+      <ProjectTiles
+        title="Leap Year Checker using nodeJS and CHalk npm package "
+        date="December 2020"
+        tileBody="Enter your birth year to check if you were born in a leap year or not."
+        smallText="For more info click below"
+      />
+      <ProjectTiles
+        title="repl.it Quiz Game"
+        date="December 2020"
+        tileBody="This is asimple General Knowledge Quiz Game with levels, users can choose the level in beginning."
+        smallText="For more info click below "
+      />
     </div>
   );
 };
